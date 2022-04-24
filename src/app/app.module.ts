961a859488c847a8147319a40ef48d62c2d5d4f0
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-04-24 10:43:39 +0100

    comment folder updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,7 +40,7 @@ import { VideoCardComponent } from './video-card/video-card.component'
 import {MatCardModule} from '@angular/material/card';
 import { CallbackComponent } from './callback/callback.component';
 import { CommentsComponent } from './comments/comments.component'
-
+import {MatMenuModule} from '@angular/material/menu';
 @NgModule({
   declarations: [
     AppComponent,
@@ -85,6 +85,7 @@ import { CommentsComponent } from './comments/comments.component'
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatMenuModule,
   ],
   providers: [
     {provide:HTTP_INTERCEPTORS, useClass:AuthInterceptor, multi:true}
